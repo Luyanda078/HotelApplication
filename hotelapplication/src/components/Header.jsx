b9a452ec49b266cebf550a995a4c83d8996b44bc
Luyanda <luyandashamase5@gmail.com>
2024-10-17 12:08:51 +0200

    Feat:Header buttons were added(Home, Accommodation & Rooms, Offers, My Bookings and Profile)

--- hotelapplication/src/components/Header.jsx
+++ hotelapplication/src/components/Header.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import './Header.css'
+import Logo from '../assets/Logo.png'
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className='Header'>
+        <img className='Header_Logo' alt='' src={Logo}/>
+        <div className="header__center"></div>
+        <div className="button">Home</div>
+        <div className="button">Accommodation & Rooms</div>
+        <div className="button">Offers</div>
+        <div className="button">My Bookings</div>
+        <div className="button">Profile</div>
+    </div>
   )
 }
 
